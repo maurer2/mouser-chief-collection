b5497ecf4fb29adf8ea575ff7a70f6f53c4b13bb
Martin <mr.mmaurer+work@gmail.com>
2020-01-01 23:32:36 +0000

    Added some types

--- scripts/get-html-markup.py
+++ scripts/get-html-markup.py
@@ -1,25 +1,27 @@
 import os
 import sys
 
+from typing import Any, IO
 from requests_html import HTMLSession
 
 URL = "https://en.wikipedia.org/wiki/Chief_Mouser_to_the_Cabinet_Office"
 
 
 def get_markup(link: str) -> str:
-    session = HTMLSession()
-    url_response = session.get(link)
+    session: Any = HTMLSession()
+    url_response: Any = session.get(link)
 
     return url_response.text
 
 
 def write_file(text: str) -> None:
-    path = os.path.join(sys.path[0], "../data/raw.htm")
-    file = open(path, "w+")
+    path: str = os.path.join(sys.path[0], "../data/raw.html")
+    file: IO = open(path, "w+")
+
     file.write(text)
     file.close()
 
 
-MARKUP = get_markup(URL)
-
-write_file(MARKUP)
+if __name__ == "__main__":
+    markup: str = get_markup(URL)
+    write_file(markup)
