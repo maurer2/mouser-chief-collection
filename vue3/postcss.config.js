03e29f385392991a6f2f97b2e5c5519bf72ef7d0
Martin <mr.mmaurer+work@gmail.com>
2020-02-21 22:54:58 +0000

    Housekeeping and update to tailwind 1.2

--- vue3/postcss.config.js
+++ vue3/postcss.config.js
@@ -1,11 +1,5 @@
-const tailwindcss = require('tailwindcss');
-const postcssNested = require('postcss-nested');
-
 module.exports = {
   plugins: {
-    // 'postcss-import': {},
-    'postcss-preset-env': {},
-    cssnano: {},
     tailwindcss: {},
     'postcss-nested': {},
   },
