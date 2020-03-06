fd5344e2d934b6fb4b2bd43a25ddbd51cc9aa41b
Martin <mr.mmaurer+work@gmail.com>
2020-03-06 23:55:37 +0000

    Grid improvements added

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="container">
+  <div class="wrapper">
     <header class="header">
-      <div class="header-inner">
-        <select-box
-          :entryNames="entryNames"
-          :active-entry="data.activeEntry"
-          @entry-selected="handleEntrySelected"
-        />
-      </div>
+      <select-box
+        :entryNames="entryNames"
+        :active-entry="data.activeEntry"
+        @entry-selected="handleEntrySelected"
+      />
     </header>
     <main class="main">
-      <div class="main-inner" v-if="data.activeEntry">
+      <template v-if="data.activeEntry">
         <pager
+          class="pager pagger--left"
           :isPrevButton="true"
           :isDisabled="buttonPrevIsDisabled"
           @pager-clicked="handlePrevClick"
         />
         <entry :active-entry="entries[data.activeEntry]" />
         <pager
+          class="pager pagger--right"
           :isPrevButton="false"
           :isDisabled="buttonNextIsDisabled"
           @pager-clicked="handleNextClick"
         />
-      </div>
+      </template>
     </main>
     <footer class="footer">
       {{ entryNames.length }} entries loaded
@@ -103,11 +103,13 @@ export default {
 </script>
 
 <style scoped lang="postcss">
-  .container {
+  .wrapper {
     display: contents;
   }
 
   .header {
+    grid-area: header;
+
     @apply
       p-4
       bg-gray;
@@ -118,77 +120,76 @@ export default {
     }
   }
 
-  .header-inner {
-    @screen md {
-      margin: auto;
-      width: calc(75% - 200px + 3rem);
-    }
-  }
-
   .main {
-    @apply
-      p-4
-      bg-gray-light;
-  }
+    grid-area: main;
 
-  .main-inner {
     display: grid;
     grid-template-rows:
-      min-content
-      1fr
-      min-content;
-    grid-template-columns:
-      1fr
+      50px
       1fr;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+        "sidebar-left sidebar-right"
+        "main main"
+      ;
 
     @screen md {
-      grid-template-rows: 1fr;
-      grid-template-columns:
-        100px
-        1fr
-        100px;
+      grid-template-rows:
+        1fr;
+      grid-template-columns: 100px 1fr 100px;
+      grid-template-areas:
+        "sidebar-left main sidebar-right"
+      ;
       min-height: 100%;
       align-items: top;
     }
-  }
-
-  .button {
-    grid-row: 1 / 2;
 
     @apply
-      p-2
-      bg-pink-2
-      text-white
-      rounded;
+      p-4
+      bg-gray-light;
 
-    @screen md {
-      grid-row: 1;
-      height: min-content;
+    .button {
+      @apply
+        p-2
+        bg-pink-2
+        text-white
+        rounded;
+
+      @screen md {
+        height: min-content;
+      }
+
+      &--is-disabled {
+        @apply
+          opacity-50
+          cursor-not-allowed;
+      }
     }
 
-    &--is-disabled {
-      @apply
-        opacity-50
-        cursor-not-allowed;
+    .pagger--left {
+      grid-area: sidebar-left;
+    }
+
+    .pagger--right {
+      grid-area: sidebar-right;
     }
   }
 
   .entry {
-    grid-row: 2 / 2;
-    grid-column: span 2;
+      @apply
+        mt-4;
 
-    @apply
-      mt-4;
-
-    @screen md {
-      grid-row: 1;
-      grid-column: 2 / 3;
-      margin: 0 auto;
-      width: 75%;
+      @screen md {
+        grid-row: 1;
+        grid-column: 2 / 3;
+        margin: 0 auto;
+        width: 75%;
+      }
     }
-  }
 
   .footer {
+    grid-area: footer;
+
     @apply
       p-2
       text-center
