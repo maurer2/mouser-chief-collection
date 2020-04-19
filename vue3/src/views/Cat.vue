0ac36e90ff97347175ab646bb71250fcd8787ef3
Martin <mr.mmaurer+work@gmail.com>
2020-04-19 22:57:41 +0200

    Fixed routing in cat

--- vue3/src/views/Cat.vue
+++ vue3/src/views/Cat.vue
@@ -6,23 +6,34 @@
 
 <script lang="ts">
 import { reactive, computed } from 'vue';
-import { router } from '../router';
 import Entry from '../components/entry/entry.vue';
 
 import entries from '../../../data/data_flattened.json';
+
+const entriesTransformedNested = Object.entries(entries).map((entry) => {
+  const [key, value] = entry;
+
+  return {
+    [key.toLowerCase()]: value,
+  };
+});
+
+const entriesTransformed = Object.assign({}, ...entriesTransformedNested);
 
 export default {
   name: 'Cat',
   components: {
     Entry,
   },
-  setup() {
-    console.log('cat');
-
+  props: {
+    entryName: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
     const data = reactive({
-      currentRoute: router.currentRoute,
-      entry: computed(() => data.currentRoute.params.entry || ''),
-      activeEntry: computed(() => entries[data.entry] || ''),
+      activeEntry: computed(() => entriesTransformed[props.entryName] || ''),
     });
 
     return {
