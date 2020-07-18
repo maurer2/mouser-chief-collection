a2a2126d5cb3d7d88c9e4842cc4deca7c3346e0e
Martin <mr.mmaurer+work@gmail.com>
2020-07-18 02:02:57 +0200

    Updated to tailwind 1.5 and updated postcss config

--- vue3/postcss.config.js
+++ vue3/postcss.config.js
@@ -1,22 +1,33 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcssNested = require('postcss-nested'); // needed for unwrapping media queries
+const postcssImport = require('postcss-import');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    'postcss-nested': {},
-    autoprefixer: {},
-    cssnano: {},
+  syntax: 'postcss-scss',
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    postcssNested,
     // https://medium.com/@kyis/vue-tailwind-purgecss-the-right-way-c70d04461475/
-    '@fullhuman/postcss-purgecss': {
-      content: [
-        './public/**/*.html',
-        './src/**/*.css',
-        './src/**/*.vue',
-      ],
+    // https://gist.github.com/LinusBorg/44b8de3455c8f3a638c4425f83fb8b12
+    purgecss({
+      content: ['./public/**/*.html', './src/**/*.css', './src/**/*.vue'],
       defaultExtractor: (content) => {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
 
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
       },
       whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/],
+    }),
+    autoprefixer,
+    cssnano,
+  ],
+  preset: {
+    autoprefixer: {
+      grid: true,
     },
   },
 };
