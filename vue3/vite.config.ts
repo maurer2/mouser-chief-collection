b64332e87fce50a8f28122416c0c7cf0533a883b
Martin <mr.mmaurer+work@gmail.com>
2020-08-20 23:47:53 +0100

    WIP

--- vue3/vite.config.ts
+++ vue3/vite.config.ts
@@ -3,19 +3,22 @@
 const path = require('path');
 import type { UserConfig } from 'vite';
 
-const srcPath = path.resolve(__dirname, '../../data/');
+const srcPath = path.resolve(__dirname, '../data/');
 
 const config: UserConfig = {
   resolvers: [{
     fileToRequest(filePath) {
       if (filePath.includes('data_flattened.json')) {
-        console.log('json', filePath, `/@/${path.relative(srcPath, filePath)}`)
-        return `/@/${path.relative(srcPath, filePath)}`;
+        console.log('data_flattened.json');
+        console.log(srcPath, filePath);
+        console.log(`/@/${path.relative(srcPath, filePath)}`)
+
+        return `/@/../../data/data_flattened.json`
       }
     },
     requestToFile (publicPath) {
-      if (publicPath.startsWith('/@/')) {
-        return path.join(srcPath, publicPath.replace(/^\/@\//, ''));
+      if (publicPath.startsWith('/@/../../data/data_flattened.json')) {
+        return path.join(srcPath, '/@/../../data/data_flattened.json');
       }
     },
   }],
