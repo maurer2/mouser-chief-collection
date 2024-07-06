71ed396b6724f8ad35a6f984cea83fc8f0a20055
Martin <mr.mmaurer@gmail.com>
2024-07-06 15:48:53 +0100

    Dict around with types

--- scripts/parse-values.py
+++ scripts/parse-values.py
@@ -4,7 +4,23 @@ import json
 import re
 import humps
 
-from typing import Any, IO
+from typing import IO, NamedTuple, TypeAlias, TypedDict
+
+MouserChiefListEntry = NamedTuple(
+    "MouserChiefListEntry",
+    name=str,
+    beganTenure=str,
+    endTenure=str,
+    primeMinisters=list[str],
+    rubbish=str,
+)
+MouserChiefList: TypeAlias = list[MouserChiefListEntry]
+
+MouserChiefListEntryType = TypedDict(
+    "MouserChiefListEntryType",
+    {"name": int, "beganTenure": str, "endTenure": str, "primeMinisters": list[str]},
+)
+MouserChiefListObject: TypeAlias = list[dict[str, MouserChiefListEntryType]]
 
 
 def get_file_contents(file_name: str) -> str:
@@ -16,7 +32,7 @@ def get_file_contents(file_name: str) -> str:
     return file_contents
 
 
-def get_keys(entries: list) -> list:
+def get_keys(entries: list[str]) -> list[str]:
     keys: list = entries[0].copy()
 
     keys.remove("Refs")
@@ -26,37 +42,35 @@ def get_keys(entries: list) -> list:
     return keys_camel_case
 
 
-def get_entries(entries: list) -> list:
+def get_entries(entries: list[str]) -> list[str]:
     entries_without_keys = entries.copy()
-
     entries_without_keys.pop(0)
 
     return entries_without_keys
 
 
-def clean_entries(entries: list) -> list:
+def clean_entry(entries: list[str]) -> MouserChiefListEntry:
     # remove [x]
-    cleaned_entries: list = [(re.sub(r"\[.*?\]", "", entry)) for entry in entries]
+    cleaned_entries = [(re.sub(r"\[.*?\]", "", entry)) for entry in entries]
 
     # trim
-    cleaned_entries: list = [entry.strip() for entry in cleaned_entries]
+    cleaned_entries = [entry.strip() for entry in cleaned_entries]
 
     # normalize commas for next step
-    cleaned_entries: list = [entry.replace(", ", ",") for entry in cleaned_entries]
+    cleaned_entries = [entry.replace(", ", ",") for entry in cleaned_entries]
 
-    # convert comma separated entries in string to real list
-    last_entry_in_list = len(cleaned_entries) - 2
-    cleaned_entries: list = [
-        entry.split(",")
-        if cleaned_entries.index(entry) == last_entry_in_list
-        else entry
-        for entry in cleaned_entries
-    ]
+    # convert comma separated entries if prime ministers to list of strings
+    cleaned_entries_as_tuple = tuple(
+        [
+            entry.split(",") if cleaned_entries.index(entry) == 3 else entry
+            for entry in cleaned_entries
+        ]
+    )
 
-    return cleaned_entries
+    return cleaned_entries_as_tuple
 
 
-def move_incumbent_to_end_of_list(entries: list) -> list:
+def move_incumbent_to_end_of_list(entries: MouserChiefList) -> MouserChiefList:
     entries_without_incumbent = [entry for entry in entries if entry[2] != "current"]
     entry_of_incumbent = [entry for entry in entries if entry[2] == "current"]
 
@@ -65,8 +79,10 @@ def move_incumbent_to_end_of_list(entries: list) -> list:
     return ordered_list
 
 
-def get_entries_mapped_with_keys(keys: list, entries: list) -> Any:
-    mapped_key_value: dict = dict(zip(keys, entries))
+def get_values_mapped_with_keys(
+    keys: list[str], entry: MouserChiefListEntry
+) -> MouserChiefListObject:
+    mapped_key_value = dict(zip(keys, entry))
 
     return mapped_key_value
 
@@ -82,22 +98,22 @@ def main() -> None:
 
     parsed_values: dict = json.loads(file_content)
 
-    entries_all: list = parsed_values["entries"]
+    entries_all: list[list[str]] = parsed_values["entries"]
 
-    keys: list = get_keys(entries_all)
-    entries: list = get_entries(entries_all)
+    keys: list[str] = get_keys(entries_all)
+    entries: list[list[str]] = get_entries(entries_all)
 
-    cleaned_entries = [clean_entries(entry) for entry in entries]
+    cleaned_entries: MouserChiefList = [clean_entry(entry) for entry in entries]
 
-    ordered_entries = move_incumbent_to_end_of_list(cleaned_entries);
+    ordered_entries: MouserChiefList = move_incumbent_to_end_of_list(cleaned_entries)
 
     mapped_entries = [
-        get_entries_mapped_with_keys(keys, entry) for entry in ordered_entries
+        get_values_mapped_with_keys(keys, entry) for entry in ordered_entries
     ]
 
     write_values_to_file(mapped_entries)
 
-    # print(mapped_entries)
+    # print('Debug list:', *mapped_entries, sep='\n- ')
 
 
 if __name__ == "__main__":
